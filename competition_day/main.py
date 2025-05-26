dd8ae1d937e714c933ab54272c8b4410de419598
Michele Brunelli <brunelli.mich@gmail.com>
2025-05-26 16:52:30 +0200

    added requirements.txt for competition_day

--- competition_day/main.py
+++ competition_day/main.py
@@ -10,6 +10,8 @@ def choose_and_run_model():
         print("2 - DINO")
         print("3 - VGG16")
         print("4 - ResNet50")
+        print("5 - Efficientnet")
+        print("6 - ResNetFinetunnato")
         choice = input("Inserisci il numero corrispondente: ").strip()
 
         if choice == "0":
@@ -23,7 +25,38 @@ def choose_and_run_model():
             script_path = os.path.join("VGG16", "main_vgg16_retrieval.py")
         elif choice == "4":
             script_path = os.path.join("ResNet", "main_resnet50_retrieval.py")
-        else:
+        elif choice == "5":
+            print("Scegli quale EfficientNet vuoi: ")
+            print("0 - Esci")
+            print("1 - EfficientNetB0")
+            print("2 - EfficientNetB4")
+            print("3 - merged EfficientNet")
+            choice2 = input("Inserisci il numero corrispondente: ").strip()
+            if choice2 == "0":
+                print("👋 Uscita.")
+                break
+            elif choice2 == "1":
+                script_path = os.path.join("image-retrieval-with-efficientnet", "EfficientNetB0.py")
+            elif choice2 == "2":
+                script_path = os.path.join("image-retrieval-with-efficientnet", "EfficientNetB4.py")
+            elif choice2 == "3":
+                script_path = os.path.join("image-retrieval-with-efficientnet", "merged_efficientNet.py")
+        elif choice == "6":
+            print("Scegli quale modello di ResNet far partire: ")
+            print("0 - Esci")
+            print("1 - L2 with Cross Entropy")
+            print("2 - Tripletloss Hard Negative Mining")
+            print("3 - Tripletloss")
+            choice3 = input("Inserisci il numero corrispondente: ").strip()
+            if choice3 == "0":
+                print("👋 Uscita.")
+                break
+            elif choice3 == "1":
+                script_path = os.path.join("ResNet-Fine-Tuning", "L2-with-CrossEntropy.py")
+            elif choice3 == "2":
+                script_path = os.path.join("ResNet-Fine-Tuning", "tripletloss-hard-negative-mining.py")
+            elif choice3 == "3":
+                script_path = os.path.join("ResNet-Fine-Tuning", "tripletloss.py")
             print("❌ Scelta non valida.")
             continue
         
