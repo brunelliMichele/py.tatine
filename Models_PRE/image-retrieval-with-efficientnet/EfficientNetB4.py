9476c55fe9e052c2c15ceb2eb633e0721db8bf05
Francesco Danesi <francesco.danesi@studenti.unitn.it>
2025-07-01 15:23:01 +0200

    efficientNetB4 fix #3

--- Models_PRE/image-retrieval-with-efficientnet/EfficientNetB4.py
+++ Models_PRE/image-retrieval-with-efficientnet/EfficientNetB4.py
@@ -7,11 +7,13 @@ import sys
 import random
 from annoy import AnnoyIndex
 import json
-from metrics import build_filename_to_class_mapping, top_k_accuracy, precision_at_k
 
 # --- Setup paths ---
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.abspath(os.path.join(BASE_DIR, "..", "..")))
 #from submit import submit
+
+from metrics import build_filename_to_class_mapping, top_k_accuracy, precision_at_k
 
 # --- EfficientNetB4 model ---
 efficientnet_model = tf.keras.applications.EfficientNetB4(
