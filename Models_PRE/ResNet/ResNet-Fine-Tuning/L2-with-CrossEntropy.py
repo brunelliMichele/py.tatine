3fe3273c13c39a815de6a5696d077f964d5984fb
Francesco Danesi <francesco.danesi@studenti.unitn.it>
2025-07-01 18:32:27 +0200

    l2crossentropy fix #3

--- Models_PRE/ResNet/ResNet-Fine-Tuning/L2-with-CrossEntropy.py
+++ Models_PRE/ResNet/ResNet-Fine-Tuning/L2-with-CrossEntropy.py
@@ -123,7 +123,7 @@ def retrieve():
         json.dump(results, f, indent=2)
     print(f"Results saved to {OUTPUT_FILE}")
 
-    submit(results, "Py.tatine")
+#    submit(results, "Py.tatine")
 
 if __name__ == '__main__':
     train()
