dd302c6e03f351cee1aaadb2242d51812abbfb2b
Michele Brunelli <brunelli.mich@gmail.com>
2025-07-01 18:07:25 +0200

    modified resnetFineTuning

--- models/manual_submission.py
+++ models/manual_submission.py
@@ -1,7 +1,7 @@
 from submit import submit
 import json
 
-with open("results/EfficientNet/B0/submission.json", "r") as f:
+with open("results/EfficientNet/merged/submission.json", "r") as f:
     results = json.load(f)
 
 submit(results, "Py.tatine")
